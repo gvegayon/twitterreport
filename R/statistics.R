7cfd11595c61c27bb3d4c1c8763fb58c5b79f397
George G Vega Yon <g.vegayon@gmail.com>
2015-07-18 11:34:14 -0700

    Improving function for stats

--- R/statistics.R
+++ R/statistics.R
@@ -1,22 +1,42 @@
  #' @description Creates a time series object with frecuencies 
  #' @param created_at POSIXct class vector
- tw_timeseries <- function(obj,created_at, units=c('secs','mins','hours','days'),span=1) {
+ tw_timeseries <- function(
+   obj,created_at, units=c('secs','mins','hours','days'),
+   nseries=5, span=1) {
    
    # Old stringAsFactors
    oldstasf <- options()$stringsAsFactors
    options(stringsAsFactors = FALSE)
    
+   # Rounding
+   if (length(units)>1) units <- 'hours'
+   created_at <- round(created_at, units)
+   
    # Processing the data
    n <- length(obj)
    tmp <- as.data.frame(do.call(rbind,lapply(1:n, function(i,...) {
-     cbind(created_at=rep(as.character(created_at[i]),length(obj[[i]])),obj=obj[[i]])
+     cbind(
+       created_at = rep(as.character(created_at[i]),length(obj[[i]])),
+       obj        = obj[[i]])
    })))
+   
+   
+   tmp <- group_by(tmp, created_at, obj)
+   tmp <- summarise(tmp, n=n())
+   tmp <- tmp[order(-tmp$n),]
+   
+   print(lapply(tmp,class))
+   # Getting the top k elements
+   series <- unique(tmp$obj)[1:nseries]
+   series <- subset(tmp, obj %in% series)
    
    options(stringsAsFactors = oldstasf)
    
    return(tmp)
  }
  
- x <- tw_extract(senate_tweets)
- y <- tw_timeseries(x$mention,senate_tweets$created_at)
- 
+ x <- tw_extract(senate_tweets$text)
+ head(tw_timeseries(x$mention,senate_tweets$created_at, units='days'), 10)
+ head(y<-tw_timeseries(x$hashtag,senate_tweets$created_at, units='days'), 10)
+ head(unique(y$obj))
+ subset(y, obj=='irandeal')
